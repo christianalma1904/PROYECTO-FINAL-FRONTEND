f12899ce55b3788ce1f50b5e9325e7006c577a8f
Christian Alcocer <christianalma001@gmail.com>
2025-07-14 17:08:09 -0500

    feat: move folders to root folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css'; // <-- Importa Bootstrap aquí primero
+import './index.css'; // <-- Tu CSS propio después (si lo tienes)
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
